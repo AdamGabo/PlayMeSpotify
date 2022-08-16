9abb309a4fea3c9be1ad560e1c658138f93c773b
Adam Gaboury <adam.gaboury@hotmail.com>
2022-08-16 15:34:02 -0400

    Geoapify Features added for MAP

--- assets/js/testGeo.js
+++ assets/js/testGeo.js
@@ -1,49 +1,41 @@
 // Adam's Code Below
 //
-//
-//grab information from the Trail API.
-//Will need to add extra function to click event which grabs the data
-//Local Storage will be a priority
-
-//grab information from the Trail API.
-//Will need to add extra function to click event which grabs the data
-//Local Storage will be a priority
-//GEOPIFY API CALLS
-var results = []; 
-var long;
-var lat;
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
-    
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-function showError(error){
-    if (error.PERMISSION_DENIED){
-      console.log("The user has denied the request for Geolocation"); 
-    }
-  }
+//GEOPIFY API CALLS, MULTIPLE APIs FROM THE "GEOAPIFY FAMILY" HAVE BEEN USED IN THE APPLICATION, STANDARD CODE (AS DEFINED IN THE API DOCUMENTATION) FOR THE SEARCH BAR HAS BEEN UTILIZED 
+//GLOBAL VARIABLE FOR THE MAP 
+var map; 
+//GET THE DEFAULT LOCATION OF THE MAP FROM LOCAL STORAGE (PREVIOUS DATA ENTRY) OR FROM IP ADDRESS LOCATION (DEFAULT FOR COUNTY - PERMISSION FROM WEB BROWSER NOT NEEDED)
 function getIPAddressLocation() {
     $.getJSON('https://geolocation-db.com/json/')
          .done (function(location) {
-            lat = location.latitude; 
-            long = location.longitude; 
-            showMap(lat,long); 
+            var lat = location.latitude; 
+            var long = location.longitude; 
+
+            //IF OLD ADDRESS IS STORED IN LOCAL STORAGE SHOW THAT ITEM INSTEAD 
+            if (localStorage.getItem("Previous Address"))
+            {
+            var oldData = (localStorage.getItem("Previous Address")); 
+            getSearchResultGeo(oldData.properties.long, oldData.properties.lat)
+            showMap(oldData.properties.lat,oldData.properties.long); 
+
+            }
+            else 
+            {
+                showMap(lat,long); 
+            }
+            
             
          });
 }
+//INTIALIZE THE MAP AND GATHER THE RASTER IMAGES FROM LEAFLET 
 function showMap(lat,long){
+  //create the map 
+  map = L.map("my-map").setView([lat, long], 14);
 
-  var map = L.map("my-map").setView([lat, long], 14)
-
-  // Get your own API Key on https://myprojects.geoapify.com
-  var myAPIKey = "1072fcb061a849c28775a0714807e737";
+  var myAPIKey = geoapifyAPIKey;
 
   // Retina displays require different mat tiles quality
   var isRetina = L.Browser.retina;
-
+  //DISPLAY URLs 
   var GeoUrl =
     "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}";
   var retinaUrl =
@@ -56,65 +48,264 @@ function showMap(lat,long){
     apiKey: myAPIKey,
     maxZoom: 20,
     id: "osm-bright",
-  }).addTo(map);
-  getSearchResultGeo(); 
+  }).addTo(map); 
 }
 
-async function getSearchResultGeo()
+//MAKE REQUEST TO GET ALL THE LOCATIONS IN A 5000m RADIUS OF THE DESIRED LOCATION
+async function getSearchResultGeo(long, lat)
 {
     $(document).ready(function () {
         $.ajax({
-            //url: "https://api.geoapify.com/v2/places?categories=leisure.park&conditions=named&filter=circle:"+long.toString()+","+lat.toString()+",5000&bias=proximity:"+long.toString()+","+lat.toString()+"&lang=en&limit=20&apiKey=1072fcb061a849c28775a0714807e737", 
-            url: "https://api.geoapify.com/v2/places?categories=leisure.park&conditions=named&filter=circle:-78.8635324,43.8975558,5000&bias=proximity:-78.8635324,43.8975558&lang=en&limit=20&apiKey=1072fcb061a849c28775a0714807e737",
+            url: `https://api.geoapify.com/v2/places?categories=leisure.park&conditions=named&filter=circle:${long},${lat},5000&bias=proximity:${long},${lat}&lang=en&limit=1&apiKey=1072fcb061a849c28775a0714807e737`,
             type: "GET", 
             success: function (result) {
                 console.log(result);
                 results = result; 
-                displayMarkers();
+                displayMarkers(results);
             }, 
             error: function (error) {
                 console.log(error); 
             }
         })
-        
 })  
-//input = encodeURI(input);
-//let url = `https://api.geoapify.com/v2/places?categories=leisure.park&conditions=named&filter=circle:${long},${lat},5000&bias=proximity:${long},${lat}&lang=en&limit=20&apiKey=${geoapifyAPIKey}`;
-// let url = "https://api.geoapify.com/v2/places?categories=leisure.park&conditions=named&filter=circle:-78.8635324,43.8975558,5000&bias=proximity:-78.8635324,43.8975558&lang=en&limit=20&apiKey=1072fcb061a849c28775a0714807e737"; 
-
-// let response = await fetch(url);
-// let json = await response.json();
-// let resultsArr = json.results;
-
-
-// localStorage.setItem(resultsArr.name, resultsArr); 
-// localStorage.setItem("Longitude", long); 
-// localStorage.setItem("Latitude", lat); 
-
-// let searchResultContainerEl = document.getElementById("geoResults");
-// resultsArr.forEach((result) => {
-//   renderSearchResult(result, type, searchResultContainerEl);
-// });
 }
 
-var i = 0; 
-function displayMarkers() {
+//DISPLAY MARKERS OF REQUESTED LOCATION WITH POPUPS
+function displayMarkers(results) {
     var markerIcon = L.icon({
-        iconUrl: `https://api.geoapify.com/v1/icon?size=xx-large&type=awesome&color=%233e9cfe&icon=paw&apiKey=${geoapifyAPIKey}`,
+        iconUrl: `https://api.geoapify.com/v1/icon?type=awesome&color=%2332e713&icon=tree&apiKey=${geoapifyAPIKey}`,
         iconSize: [31, 46], // size of the icon
         iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
         popupAnchor: [0, -45] // point from which the popup should open relative to the iconAnchor
       });
-
+    //FOR LOOP WHICH GOES THROUGH ALL THE PARKS IN THE AREA 
     for (var i=0; i < results.features.length; i++)
     {
-        var zooMarker = L.marker([results[i].features.i.properties.long, item.features.i.properties.lat], {
+        var geoLong = results.features[i].properties.lon; 
+        var geoLat = results.features[i].properties.lat; 
+
+        var markerPopup = L.popup().setContent(results.features[i].properties.address_line1 + ", " + results.features[i].properties.address_line2); 
+        var marker = L.marker([geoLat,geoLong], {
         icon: markerIcon
-    }).addTo(map);
+    }).bindPopup(markerPopup).addTo(map);
     }
 }
+//CREATE SEARCH BAR FOR USER AFTER DEFAULT HAS BEEN SET, STANDARD GEOAPIFY API CODE HAS BEEN USED FOR THIS FEATURE source:https://apidocs.geoapify.com/samples/autocomplete/autocomplete-tutorial/#step-1
+/* 
+	The addressAutocomplete takes as parameters:
+  - a container element (div)
+  - callback to notify about address selection
+  - geocoder options:
+  	 - placeholder - placeholder text for an input element
+     - type - location type
+*/
+function addressAutocomplete(containerElement, callback, options) {
+    // create input element
+    var inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "text");
+    inputElement.setAttribute("placeholder", options.placeholder);
+    containerElement.appendChild(inputElement);
+  
+    // add input field clear button
+    var clearButton = document.createElement("div");
+    clearButton.classList.add("clear-button");
+    addIcon(clearButton);
+    clearButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      inputElement.value = '';
+      callback(null);
+      clearButton.classList.remove("visible");
+      closeDropDownList();
+    });
+    containerElement.appendChild(clearButton);
+  
+    /* Current autocomplete items data (GeoJSON.Feature) */
+    var currentItems;
+  
+    /* Active request promise reject function. To be able to cancel the promise when a new request comes */
+    var currentPromiseReject;
+  
+    /* Focused item in the autocomplete list. This variable is used to navigate with buttons */
+    var focusedItemIndex;
+  
+    /* Execute a function when someone writes in the text field: */
+    inputElement.addEventListener("input", function(e) {
+      var currentValue = this.value;
+  
+      /* Close any already open dropdown list */
+      closeDropDownList();
+  
+      // Cancel previous request promise
+      if (currentPromiseReject) {
+        currentPromiseReject({
+          canceled: true
+        });
+      }
+  
+      if (!currentValue) {
+        clearButton.classList.remove("visible");
+        return false;
+      }
+  
+      // Show clearButton when there is a text
+      clearButton.classList.add("visible");
+  
+      /* Create a new promise and send geocoding request */
+      var promise = new Promise((resolve, reject) => {
+        currentPromiseReject = reject;
+  
+        var apiKey = geoapifyAPIKey;
+        var url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(currentValue)}&limit=5&apiKey=${apiKey}`;
+        
+        if (options.type) {
+            url += `&type=${options.type}`;
+        }
+  
+        fetch(url)
+          .then(response => {
+            // check if the call was successful
+            if (response.ok) {
+              response.json().then(data => resolve(data));
+            } else {
+              response.json().then(data => reject(data));
+            }
+          });
+      });
+  
+      promise.then((data) => {
+        currentItems = data.features;
+  
+        /*create a DIV element that will contain the items (values):*/
+        var autocompleteItemsElement = document.createElement("div");
+        autocompleteItemsElement.setAttribute("class", "autocomplete-items");
+        containerElement.appendChild(autocompleteItemsElement);
+  
+        /* For each item in the results */
+        data.features.forEach((feature, index) => {
+          /* Create a DIV element for each element: */
+          var itemElement = document.createElement("DIV");
+          /* Set formatted address as item value */
+          itemElement.innerHTML = feature.properties.formatted;
+  
+          /* Set the value for the autocomplete text field and notify: */
+          itemElement.addEventListener("click", function(e) {
+            inputElement.value = currentItems[index].properties.formatted;
+  
+            callback(currentItems[index]);
+  
+            /* Close the list of autocompleted values: */
+            closeDropDownList();
+          });
+  
+          autocompleteItemsElement.appendChild(itemElement);
+          
+        });
+      }, (err) => {
+        if (!err.canceled) {
+          console.log(err);
+        }
+      });
+    });
+  
+    /* Add support for keyboard navigation */
+    inputElement.addEventListener("keydown", function(e) {
+      var autocompleteItemsElement = containerElement.querySelector(".autocomplete-items");
+      if (autocompleteItemsElement) {
+        var itemElements = autocompleteItemsElement.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          e.preventDefault();
+          /*If the arrow DOWN key is pressed, increase the focusedItemIndex variable:*/
+          focusedItemIndex = focusedItemIndex !== itemElements.length - 1 ? focusedItemIndex + 1 : 0;
+          /*and and make the current item more visible:*/
+          setActive(itemElements, focusedItemIndex);
+        } else if (e.keyCode == 38) {
+          e.preventDefault();
+  
+          /*If the arrow UP key is pressed, decrease the focusedItemIndex variable:*/
+          focusedItemIndex = focusedItemIndex !== 0 ? focusedItemIndex - 1 : focusedItemIndex = (itemElements.length - 1);
+          /*and and make the current item more visible:*/
+          setActive(itemElements, focusedItemIndex);
+        } else if (e.keyCode == 13) {
+          /* If the ENTER key is pressed and value as selected, close the list*/
+          e.preventDefault();
+          if (focusedItemIndex > -1) {
+            closeDropDownList();
+          }
+        }
+      } else {
+        if (e.keyCode == 40) {
+          /* Open dropdown list again */
+          var event = document.createEvent('Event');
+          event.initEvent('input', true, true);
+          inputElement.dispatchEvent(event);
+        }
+      }
+    });
+  
+    function setActive(items, index) {
+      if (!items || !items.length) return false;
+  
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove("autocomplete-active");
+      }
+  
+      /* Add class "autocomplete-active" to the active element*/
+      items[index].classList.add("autocomplete-active");
+  
+      // Change input value and notify
+      inputElement.value = currentItems[index].properties.formatted;
+      callback(currentItems[index]);
+    }
+  
+    function closeDropDownList() {
+      var autocompleteItemsElement = containerElement.querySelector(".autocomplete-items");
+      if (autocompleteItemsElement) {
+        containerElement.removeChild(autocompleteItemsElement);
+      }
+  
+      focusedItemIndex = -1;
+    }
+  
+    function addIcon(buttonElement) {
+      var svgElement = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+      svgElement.setAttribute('viewBox', "0 0 24 24");
+      svgElement.setAttribute('height', "24");
+  
+      var iconElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+      iconElement.setAttribute("d", "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z");
+      iconElement.setAttribute('fill', 'currentColor');
+      svgElement.appendChild(iconElement);
+      buttonElement.appendChild(svgElement);
+    }
+    
+      /* Close the autocomplete dropdown when the document is clicked. 
+        Skip, when a user clicks on the input field */
+    document.addEventListener("click", function(e) {
+      if (e.target !== inputElement) {
+        closeDropDownList();
+      } else if (!containerElement.querySelector(".autocomplete-items")) {
+        // open dropdown list again
+        var event = document.createEvent('Event');
+        event.initEvent('input', true, true);
+        inputElement.dispatchEvent(event);
+      }
+    });
+  
+  }
+    //SET THE DATA FROM USER SELECTION 
+    addressAutocomplete(document.getElementById("autocomplete-container"), (data) => {
+    console.log("Selected option: ");
+    console.log(data);
 
+    //LOAD IN MARKERS FROM SEARCH RESULT
+    getSearchResultGeo(data.properties.lon, data.properties.lat); 
+    //MOVE MAP TO DESIRED LOCATION
+    map.setView([data.properties.lat, data.properties.lon], 14)
+    localStorage.setItem("Previous Address", data.properties);
+
+  }, {
+      placeholder: "Enter an address here"
+  });
+  
+//FIRST INTIALIZATION FOR MAP API FEATURE
 getIPAddressLocation();
-
-
-// if statement that filetes out location requests https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
